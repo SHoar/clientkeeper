731e8ec24b486b542c911bc6f8a6fd6530f81c31
Sean Hoar <sean.hoar@gmail.com>
2016-11-16 01:49:18 -0500

    attach AppController scope in index.html

--- public/controllers/AppController.js
+++ public/controllers/AppController.js
@@ -1,3 +1,8 @@
 var myApp = angular.module('myApp', [])
 
-myApp.controller('AppController')
+myApp.controller('AppController', [ '$scope', '$http', '$location', function( $scope, $http, $location){
+  $http.get('/clients').success(function (response) {
+    console.log('Data received from server');
+    $scope.clients = response
+  })
+}])
